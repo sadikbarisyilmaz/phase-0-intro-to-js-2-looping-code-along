8e06242ffb0a9677373c43d2864848fd6e6be5d4
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-04 11:56:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const writeCards = (arr, greeting) => {
+   const msgArr = []
+    for (let i = 0; i < arr.length; i++) {
+        msgArr.push(`Thank you, ${arr[i]}, for the wonderful ${greeting} gift!`) 
+    }
+    return msgArr
+}
+
+
+const countDown = (num) => {
+    while (num >= 0) {
+        console.log(num);
+        num--
+    }
+}
+
+
